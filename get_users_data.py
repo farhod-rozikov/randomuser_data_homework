cfa6ddbd57c37f317d86e474c5d02695b5201425
Farhod Rozikov <fara.rozikov@gmail.com>
2023-01-21 17:54:23 +0500

    get users data

--- get_users_data.py
+++ get_users_data.py
@@ -14,7 +14,7 @@ def get_users_data(data:dict) -> list:
         list: users data list
     """
     i = 0
-    users_data = [{"first_name": u_d['name']['first'], "last_name": u_d['name']['last'], "phone_number": u_d['phone']}  for u_d in data]
+    users_data = [{"first_name": u_d['name']['first'], "last_name": u_d['name']['last'], "phone_number": u_d['phone']}  for u_d in data['results']]
     return users_data
 data = get_data.get_data('randomuser_data.json')
 print(get_users_data(data))
